d0bc3c2c66b5654f79b16880dd3cb8c2c1122c48
kimpandol <yesjisu5595@gmail.com>
2022-08-05 22:29:45 +0200

    modified:   .gitignore 	modified:   english test/main.py

--- english test/main.py
+++ english test/main.py
@@ -6,13 +6,15 @@ BTN_COLOR = "#F0F0F0"
 import csv
 from tkinter import *
 import random
+import pandas as pd
 from english_test_func import *
 
-answer = 0
+
 
 #문제 출력
 def next_question():
     global answer
+    global cur_question
 
     for i in range(4):
         button[i].config(bg=BTN_COLOR)
@@ -33,33 +35,43 @@ def next_question():
 
 #정답 체크
 def check_answer(idx):
+    print(cur_question)
     idx = int(idx)
     if answer == idx:
         button[idx].config(bg=CORRECT_COLOR)
         window.after(1000, next_question)
+
+        #정답 pandas로 읽고 맞으면 +1
+        question_pd.loc[question_pd.단어==cur_question, 'right']+=1
+        question_pd.to_csv("english test\\단어장\\voca.csv", index=False)
+
+
     else:        
         button[idx].config(bg=WRONG_COLOR)
 
 
 with open("english test\\단어장\\voca.csv", "r", encoding="UTF-8-sig") as file:
-    question = list(csv.reader(file))
+    question = list(csv.reader(file))    
+    
+with open("english test/단어장/voca.csv", "r", encoding="UTF-8-sig") as file_pd:
+    question_pd = pd.read_csv(file_pd)
 
 window = Tk()
 window.title("영퀴")
 window.config(padx=30, pady=10, bg=BGCOLOR)
 
-#question BOX
+#문제 출력 // 박스 그리기
 question_label = Label(window, width=20, height=2, text="test", font=("함초롬돋움",25,"bold"), bg=BGCOLOR, fg="white")
 question_label.pack(pady=30)
 
-#multiple choice BOX
+#4지선다 출력 // 박스 그리기
 button = []
 for i in range(4):
     btn = Button(window, text=f"{i}번", width=50, height=2, font=("함초롬돋움",15,"bold"), bg=BTN_COLOR, command=lambda idx = i:check_answer(idx))
     btn.pack()
     button.append(btn)
 
-#next btn BOX
+#다음 문제 버튼 출력 // 박스 그리기
 next_btn = Button(window, text="다음 문제", width=15, height=2, font=("함초롬돋움",15,"bold"), bg=CORRECT_COLOR, command=next_question)
 next_btn.pack(pady=30)
 
